105014ac9c1db93dfa4cbd428c51331f21d4b3df
pathoschild <pathoschild+github@gmail.com>
2012-05-09 15:16:43 -0400

    bugfix: ReSharper doesn't recognize [NotNull] unless there's a [CanBeNull] in the same namespace.

---
+++ Pathoschild.DesignByContract/CanBeNullAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Pathoschild.DesignByContract.Framework;
+
+namespace Pathoschild.DesignByContract
+{
+	/// <summary>Indicates that the value may be <c>null</c>.</summary>
+	/// <remarks>This annotation is required for ReSharper to recognize <see cref="NotNullAttribute"/>.</remarks>
+	[AttributeUsage((AttributeTargets)(ConditionTargets.Parameter | ConditionTargets.ReturnValue))]
+	public sealed class CanBeNullAttribute : Attribute { }
+}
