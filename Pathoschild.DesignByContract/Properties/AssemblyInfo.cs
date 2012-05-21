98116cccea9ea077d0a569fb13c177ca2dcdb86b
pathoschild <pathoschild+github@gmail.com>
2012-05-21 17:44:31 -0400

    created NuGet package.

--- Pathoschild.DesignByContract/Properties/AssemblyInfo.cs
+++ Pathoschild.DesignByContract/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Pathoschild.DesignByContract")]
-[assembly: AssemblyDescription("A high-performance aspect-oriented argument validation framework built on top of the free edition of PostSharp.")]
+[assembly: AssemblyDescription("A high-performance aspect-oriented argument validation framework built on top of the free edition of PostSharp which enables design-by-contract preconditions and postconditions.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright © Jesse Plamondon-Willard 2012")]
+[assembly: AssemblyCopyright("Copyright Jesse Plamondon-Willard 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
