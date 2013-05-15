ebb77befb450e3627501bca0a905f4606840ace8
Jesse Plamondon-Willard <plamondon@brainbankinc.com>
2013-05-14 22:30:09 -0400

    bumped version; +GlobalAssemblyInfo for simpler assembly versioning.

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pathoschild.DesignByContract")]
+[assembly: AssemblyCopyright("Copyright Jesse Plamondon-Willard 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.1.0.0")]
